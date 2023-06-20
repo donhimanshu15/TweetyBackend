4c3a56aaf6512e81e822d37a26f8ad33cc3918bb
Himanshu jain <77209809+donhimanshu15@users.noreply.github.com>
2023-06-20 21:51:02 +0530

    update

--- verifyToken.js
+++ verifyToken.js
@@ -3,12 +3,19 @@ import { handleError } from "./error.js";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
-
-  if (!token) return next(handleError(401, "You are not authenticated"));
+  console.log(token, "1");
+  if (!token) {
+    console.log(token, "2");
+    return next(handleError(401, "You are not authenticated"));
+  }
 
   jwt.verify(token, process.env.JWT, (err, user) => {
-    if (err) return next(createError(403, "Token is invalid"));
+    if (err) {
+      console.log("3");
+      return next(createError(403, "Token is invalid"));
+    }
     req.user = user;
+    console.log("4");
     next();
   });
 };
